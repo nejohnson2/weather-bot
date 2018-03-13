5e83070a8384dd6f6570d46ea2327053a3f9b490
Nicholas Johnson <nejohnson2@users.noreply.github.com>
2018-03-13 14:53:31 -0400

    Update weather_bot.py

--- weather_bot.py
+++ weather_bot.py
@@ -1,4 +1,5 @@
 import sys, os
+import time
 import datetime
 import requests
 import pandas as pd
@@ -15,29 +16,40 @@ def main(start_date, end_date, fpath):
 	apiKey = 'XXXXXXXXXXXX'
 	base_url = 'http://api.wunderground.com/api/{}/history_{}/q/KNYC.json'
 
+	# -- get start and end dates
 	sdate = pd.to_datetime(start_date)
 	edate = pd.to_datetime(end_date)
 
 	# -- for each day
 	for i in range((edate - sdate).days):
-
+		# -- build query
 		query = base_url.format(apiKey, sdate.strftime('%Y%m%d'))
 		print 'Weatherbot: fetching {}'.format(query)
-    
+    		
+		# -- make request to WU API
 		r = requests.get(query)
-		
+	
+		# -- convert to dataframe		
 		df = pd.DataFrame(r.json()['history']['observations'])
 
 		df['date'] = df['date'].map(lambda x: x['pretty'])
 		
+		# -- check if the file already exists
 		file_exists = os.path.isfile(fpath)
 		with open(fpath,"a") as f:
+			# -- create new file with header if necessary
 			if not file_exists:
 				df.to_csv(f, header=True, index=False)
 
+			# -- append to exisiting file
 			df.to_csv(f, header=False, index=False)
 		
+		# -- increment the start date by one day
 		sdate += datetime.timedelta(days=1)
+		
+		# -- Weather Underground API accepts only 10 calls per minute
+		time.sleep(10)
+		
 	print "Finished!"
 
 if __name__ == '__main__':
